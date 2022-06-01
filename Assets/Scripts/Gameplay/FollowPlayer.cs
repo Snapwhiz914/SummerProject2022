f73a26f69a2a429e7a83a94c7d547fd46d7aec5a
Snapwhiz914 <asilversind@gmail.com>
2022-06-01 18:37:01 -0400

    background image moves slightly based on movement from start
    it be cool

--- Assets/Scripts/Gameplay/FollowPlayer.cs
+++ Assets/Scripts/Gameplay/FollowPlayer.cs
@@ -7,21 +7,25 @@ public class FollowPlayer : MonoBehaviour
 
     public GameObject target;
     public Transform background;
+    public float backgroundMovementFactor;
     private float yStart;
     private float cameraSize;
+    private Vector2 currentBackgroundOffset;
+    private Vector3 cameraStart;
 
     void Start()
     {
         cameraSize = GetComponent<Camera>().orthographicSize;
         yStart = transform.position.y - cameraSize;
-
+        cameraStart = transform.position;
     }
     // Update is called once per frame
     void FixedUpdate()
     {
         float newY = target.transform.position.y;
         if (target.transform.position.y - cameraSize < yStart) newY = 0;
+        currentBackgroundOffset = (cameraStart - transform.position)/backgroundMovementFactor;
         transform.position = new Vector3(target.transform.position.x, newY, -10);
-        background.position = new Vector3(transform.position.x, transform.position.y, 0);
+        background.position = new Vector2(transform.position.x, transform.position.y) - currentBackgroundOffset;
     }
 }
